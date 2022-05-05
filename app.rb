7c75d685acd38eee946abbe68c522406407928db
sjtinsley <simonjtinsley@gmail.com>
2022-05-05 17:39:11 +0100

    All green baby

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'player'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -13,14 +14,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect "/play"
   end
 
   get "/play" do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb :play
   end
 
