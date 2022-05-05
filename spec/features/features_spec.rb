cecff2f0078c80e459d636c6d5a23a76273f3e39
sjtinsley <simonjtinsley@gmail.com>
2022-05-05 15:58:19 +0100

    Added feature test for name entry

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -9,3 +9,14 @@ feature "homepage behaves as expected" do
 
 end
 
+feature "players can enter names" do
+  scenario "players enters their name and can see it" do
+    visit("/")
+    page.fill_in 'player_1_name', with: 'Player-1-Name'
+    page.fill_in 'player_2_name', with: 'Player-2-Name'
+    click_button 'Submit'
+    expect(page).to have_content("Player-1-Name").and have_content("Player-2-Name")
+  end
+end
+
+
