eae800a44f84f0d373e6b040c9430d31374912c2
timbo-graham <tmgraham29@gmail.com>
2022-05-05 16:37:42 +0100

    Used session variables and redirected to a get /play route

--- app.rb
+++ app.rb
@@ -6,8 +6,22 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get "/" do
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,21 +1,18 @@
-feature "homepage behaves as expected" do
-
-  scenario "homepage says \"Testing infrastructure working!\"" do
-  
-    visit("/")
-    expect(page).to have_content("Testing infrastructure working!")
-
-  end
-
-end
-
 feature "players can enter names" do
-  scenario "players enters their name and can see it" do
+  scenario "player 1 enters their name and can see it" do
     visit("/")
     page.fill_in 'player_1_name', with: 'Player-1-Name'
     page.fill_in 'player_2_name', with: 'Player-2-Name'
     click_button 'Submit'
-    expect(page).to have_content("Player-1-Name").and have_content("Player-2-Name")
+    expect(page).to have_content("Player-1-Name")
+  end
+
+  scenario "player 2 enters their name and can see it" do
+    visit("/")
+    page.fill_in 'player_1_name', with: 'Player-1-Name'
+    page.fill_in 'player_2_name', with: 'Player-2-Name'
+    click_button 'Submit'
+    expect(page).to have_content("Player-2-Name")
   end
 end
 
