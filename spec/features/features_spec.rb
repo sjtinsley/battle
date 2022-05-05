016ae463fbc8dcab36156081fe777b3547e4803c
timbo-graham <tmgraham29@gmail.com>
2022-05-05 16:42:21 +0100

    Added test for player 1 to see player 2's hitpoints

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -16,4 +16,15 @@ feature "players can enter names" do
   end
 end
 
+feature "players can see each other's hit points" do
+  scenario "player 1 can see player 2's hitpoints" do
+    visit("/")
+    page.fill_in 'player_1_name', with: 'Player-1-Name'
+    page.fill_in 'player_2_name', with: 'Player-2-Name'
+    click_button 'Submit'
+    expect(page).to have_content("Player 2: 100HP")
+  end
+
+
+end
 
